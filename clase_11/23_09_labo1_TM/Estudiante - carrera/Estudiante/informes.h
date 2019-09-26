86bc6bcc48b9053e7b225181e0817174071b0412
alumno <alumno@lab6pc00>
2019-09-26 19:58:33 -0300

    clase12

---
+++ clase_11/23_09_labo1_TM/Estudiante - carrera/Estudiante/informes.h
@@ -0,0 +1,11 @@
+#ifndef INFORMES_H_INCLUDED
+#define INFORMES_H_INCLUDED
+
+
+int Informes_listarPorCriterio(Tipo arrayA, Tipo arrayB, int sizeI, int sizeJ, char* criterio);
+int Informes_listarCriterioContadorAcumulado(Tipo arrayA, Tipo arrayB, int sizeI, int sizeJ);
+int Informes_maxContadorAcumulado(Tipo arrayA, Tipo arrayB, int sizeI, int sizeJ);
+
+
+
+#endif // INFORMES_H_INCLUDED
