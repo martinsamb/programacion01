38f8468aef3b14b829b83d95a997de923213e226
alumno <alumno@lab6pc00>
2019-09-26 22:11:41 -0300

    clase12

--- clase_12/01_Parcial_01/main.c
+++ clase_12/01_Parcial_01/main.c
@@ -10,35 +10,39 @@
 int main()
 {
     int opcion;
-    int contadorIdfantasma=0;                   //cambiar
+    int contadorIdAsociado=0;
+    int contadorIdLlamada = 0;                  //cambiar
 
-    Fantasma arrayFantasma[QTY_TIPO];                   //cambiar
-    fantasma_Inicializar(arrayFantasma,QTY_TIPO);                   //cambiar
+    asociado arrayAsociado[QTY_TIPO];                   //cambiar
+    asociado_Inicializar(arrayAsociado,QTY_TIPO);
+
+    llamada arrayLlamada[QTY_TIPO];                   //cambiar
+    llamada_Inicializar(arrayLlamada,QTY_TIPO);                 //cambiar
 
     do
     {
-        utn_getUnsignedInt("\n\n1) Alta \n2) Modificar \n3) Baja \n4) Listar \n5) Ordenar \n6) Salir\n",                   //cambiar
+        utn_getUnsignedInt("\n\n1) Alta Asociado \n2) Modificar datos Asociado \n3) Baja Asociado \n4) Nueva Llamada \n5) Asignar Ambulancia \n6) Salir\n",                   //cambiar
                       "\nError",1,sizeof(int),1,11,1,&opcion);
         switch(opcion)
         {
             case 1: //Alta
-                fantasma_alta(arrayFantasma,QTY_TIPO,&contadorIdfantasma);                   //cambiar
+                asociado_alta(arrayAsociado,QTY_TIPO,&contadorIdAsociado);                   //cambiar
                 break;
 
             case 2: //Modificar
-                fantasma_modificar(arrayFantasma,QTY_TIPO);                   //cambiar
+                asociado_modificar(arrayAsociado,QTY_TIPO);                   //cambiar
                 break;
 
             case 3: //Baja
-                fantasma_baja(arrayFantasma,QTY_TIPO);                   //cambiar
+                asociado_baja(arrayAsociado,QTY_TIPO);                   //cambiar
                 break;
 
             case 4://Listar
-                fantasma_listar(arrayFantasma,QTY_TIPO);                   //cambiar
+                llamada_alta(arrayLlamada,QTY_TIPO,&contadorIdLlamada);                   //cambiar
                 break;
 
             case 5://Ordenar
-                fantasma_ordenarPorString(arrayFantasma,QTY_TIPO);                   //cambiar
+                llamada_modificar(arrayLlamada,QTY_TIPO);                   //cambiar
                 break;
 
             case 6://Salir
