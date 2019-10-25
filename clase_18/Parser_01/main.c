ca6d27d47a64d32a6fe30281f33f0c7fe3bd93b1
alumno <alumno>
2019-10-24 22:04:55 -0300

    parser

---
+++ clase_18/Parser_01/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    Cliente* listClientes[1000];//obtengo un array de puntero de tipo clientes
+
+ FILE *pFile;
+ int r;
+ char var1[50],var3[50],var2[50],var4[50];
+ pFile = fopen("clientes.txt","r");
+ if(pFile == NULL)
+ {
+ printf("El archivo no existe");
+ exit(EXIT_FAILURE);
+ }
+ do
+ {
+ r = fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n]\n",var1,var2,var3,var4);
+ if(r==4)
+ {
+     printf("Lei: %s %s %s %s\n",var1,var2,var3,var4);
+     //Hacer un new para generar un cliente
+     //cargar los campos con los datos
+     //guardar las addr en la lista
+     int id = atoi(var1);
+     Cliente* pc = new_clienteParametros(var2,var4,var5,var3,i,1);
+     if(pc! = NULL)
+     {
+         listaClientes[posicion] = pc;
+         posicion++;
+         if(posicion>=1000)
+            break;
+     }
+
+ }
+ else
+ break;
+ }while(!feof(pFile));
+ fclose(pFile);
+ exit(EXIT_SUCCESS);
+}
